b4182777a65984b7faeb88095b57f6f693a10ef9
Aleksei Anisimov <suxelshock@gmail.com>
2024-03-26 19:21:36 +0300

    Init commit

---
+++ app/src/main/java/ru/yandex/practicum/sprint09koh15/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.sprint09koh15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
