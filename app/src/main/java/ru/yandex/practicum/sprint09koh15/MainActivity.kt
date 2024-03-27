ccf7fc8b9ddbd30ce970a747770e115c28e22fdd
Aleksei Anisimov <suxelshock@gmail.com>
2024-03-27 11:13:47 +0300

    Настройка edittext

--- app/src/main/java/ru/yandex/practicum/sprint09koh15/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint09koh15/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +27,37 @@ class MainActivity : AppCompatActivity() {
 
         Utils.vibrate(this)
         vibrate()
+
+        findViewById<EditText>(R.id.edit_text).apply {
+            addTextChangedListener(PhoneNumberFormattingTextWatcher())
+//            addTextChangedListener(object : TextWatcher {
+//
+//                var selfEdit = false
+//
+//                override fun beforeTextChanged(
+//                    s: CharSequence?,
+//                    start: Int,
+//                    count: Int,
+//                    after: Int
+//                ) {
+//
+//                }
+//
+//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                    Log.d("SPRINT_9_MAIN", "onTextChanged s=$s start=$start before=$before count=$count")
+//                }
+//
+//                override fun afterTextChanged(s: Editable?) {
+//                    Log.d("SPRINT_9_MAIN", "afterTextChanged s=$s")
+//                    if (!selfEdit) {
+//                        selfEdit = true
+//                        s?.append("P")
+//                        selfEdit = false
+//                    }
+//                }
+//
+//            })
+        }
 
     }
 
